37ac594aebcdfd4e4e047dd475ad53b7623eefdd
Caio Tavares <69085383+caiotaavares@users.noreply.github.com>
2022-05-12 15:55:45 -0300

    fix

---
+++ Signup.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Signup extends CI_Controller {
+
+	public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('users_model');
+    }
+	
+	public function index()
+	{
+		$data['title'] = "Signup - CodeIgniter";
+		$this->load->view('pages/signup', $data);
+	}
+
+	// Cadastra o usuário
+	public function store()
+	{
+		$user = array(
+			"name" => $this->input->post("name"),
+			"country" => $_POST["country"], 
+			"email" => $_POST["email"],
+			// md5 gera o hash da conta
+			"password" => md5($_POST["password"])
+		);
+		// usa store pra cadastrar o usuário no banco
+		$this->users_model->store($user);
+		redirect("login");
+	}
+}
