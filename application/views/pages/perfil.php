8941946e1d6dbaa935c8eb18f7250496fac30f9a
caiotaavares <caio_s.tavares2@hotmail.com>
2022-05-12 22:18:22 -0300

    user-avatar fix

--- application/views/pages/perfil.php
+++ application/views/pages/perfil.php
@@ -10,7 +10,7 @@
 		<div>
 			<!-- BUG aprender a pegar imagem do db e por aqui! -->
 			<?php if(!empty($_SESSION['logged_user']['user_avatar'])) { ?>
-				<img src="<?= $_SESSION['logged_user']['user_avatar'] ?>" width="50" height="50">
+				<img src="<?= $_FILES['logged_user']['user_avatar'] ?>" width="50" height="50">
 			<?php } else { ?>
 				<img src="<?= base_url()?>assets/images/user-avatar.png" width="350" height="350""/>
 			<?php } ?>
